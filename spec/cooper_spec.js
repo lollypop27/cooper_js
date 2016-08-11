a1774d320c394b7a9b529f2f2ab7d4db7b3f6edc
lollypop27 <lara@adfactory.co.za>
2016-08-11 17:58:01 +0200

    fixing include to person js file, adding person function

---
+++ spec/cooper_spec.js
@@ -0,0 +1,7 @@
+describe("cooperAssessmentOf", function() {
+  var person;
+
+  beforeEach(function() {
+    male = new Person({ gender: 'male', age: 13 });
+    female = new Person({ gender: 'female', age: 13 });
+  });
---
+++ spec/person_spec.js
@@ -0,0 +1,8 @@
+describe("Person", function() {
+    var person;
+
+    beforeEach(function() {
+        person = new Person({ gender: 'male', age: 36 });
+    });
+
+});
